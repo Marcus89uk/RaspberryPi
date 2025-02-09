ef03ed49a44295ab22792518d84cb5d5d1cb7b63
Marcus89uk <marcus89uk@outlook.com>
2025-02-09 18:33:43 +0000

    Added  retroPie install.sh

---
+++ retropi.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade
+cd Downloads
+git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
+cd RetroPie-Setup
+chmod +x retropie_setup.sh
+sudo ./retropie_setup.sh
