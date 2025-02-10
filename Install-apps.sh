83b1a47e597ab58d828f54aa13c24282fc308215
Marcus <marcus89uk@outlook.com>
2025-02-10 05:13:17 +0000

    add

--- Install-apps.sh
+++ Install-apps.sh
@@ -2,7 +2,7 @@
 
 mkdir ~/RaspberryPi
 cd ~/RaspberryPi
-
+sudo mkdir /.raspi-backups
 
 sudo apt install curl flatpak -y
 
@@ -12,20 +12,25 @@ flatpak install org.desmume.DeSmuME io.github.flattool.Warehouse
 
 
 curl -o https://raw.githubusercontent.com/Marcus89uk/RaspberryPi/refs/heads/main/custom-files/apt-sources/sources.list
+sudo cp -R /etc/apt /.raspi-backups/
 sudo cp sources.list /etc/apt/
 
 
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 
+curl -o https://raw.githubusercontent.com/Marcus89uk/RaspberryPi/refs/heads/main/images/ps2.png
 curl -o https://archive.org/download/AetherSX2-Collection/AetherSX2-archive/desktop/linux/AetherSX2-v1.0-2370.AppImage
+curl -o https://raw.githubusercontent.com/Marcus89uk/RaspberryPi/refs/heads/main/backup-files/app-launchers/aethersx2.desktop
 
 mkdir ~/.emulator
 mkdir ~/.emulator/ps2
-mv AetherSX2-archive/desktop/linux/AetherSX2-v1.0-2370.AppImage  ~/.emulator/ps2/
+mv AetherSX2-v1.0-2370.AppImage  ~/.emulator/ps2/
+mv ps2.png ~/.emulator/ps2/
+mv aethersx2.desktop ~/.local/share/applications/
 
 curl - o https://download.remotegamepad.com/remotegamepad_arm64.deb
 
 sudo apt install ./remotegamepad_arm64.deb -y
 
-sudo echo "deb https://fasttrack.debian.net/debian-fasttrack/ bookworm-fasttrack main contrib" | tee -a /etc/apt/sources.list >/dev/null
+
 
